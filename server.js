67f18609239b3545673cf340a88e6ba0bd86f9b1
vipulsaxena5503 <vipulsaxena.saxena@gmail.com>
2024-05-14 13:02:31 -0700

    updated

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const jsonServer = require('json-server');
 const cors = require('cors');
 const server = jsonServer.create();
 const path = require('path');
+console.log("__dirname is:", __dirname);
+
 const router = jsonServer.router(path.join(__dirname, 'db.json')); // Path to the db.json file
+
+console.log("router is:", router);
+
 const middlewares = jsonServer.defaults();
 
 // CORS options—set to allow all origins for testing purposes
@@ -15,7 +20,9 @@ server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(router);
 
+  
 const port = process.env.PORT || 3000; // Use PORT environment variable or default to 3000
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`JSON Server is running on port ${port}`);
 });
+
